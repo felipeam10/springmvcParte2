3d42eb78432a390a95eb26a9090e514f7090fdbb
felipeam10 <felipeam10@hotmail.com>
2021-06-03 12:45:22 -0300

    commit  aula_2.1

--- src/main/java/br/com/felipe/mvc/sprgmvc2/WebSecurityConfig.java
+++ src/main/java/br/com/felipe/mvc/sprgmvc2/WebSecurityConfig.java
@@ -1,19 +1,27 @@
 package br.com.felipe.mvc.sprgmvc2;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -27,18 +35,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		    )
 			.logout(logout -> logout.logoutUrl("/logout"));
 	}
-
-	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("joao")
-				.password("joao")
-				.roles("ADM")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
-	}
 	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		/*
+		UserDetails user = 
+				User.builder()
+					.username("maria")
+					.password(encoder.encode("maria"))
+					.roles("ADM")
+					.build();
+		*/
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.passwordEncoder(encoder);
+//			.withUser(user);
+	}
+
 }
