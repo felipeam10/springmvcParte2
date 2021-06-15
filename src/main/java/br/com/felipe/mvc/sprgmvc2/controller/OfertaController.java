4ebd205ac49225707e152374d1a5520932ecffc7
felipeam10 <felipeam10@hotmail.com>
2021-06-15 00:05:00 -0300

    commit aula 4.2 implementando o Rest Controller

---
+++ src/main/java/br/com/felipe/mvc/sprgmvc2/controller/OfertaController.java
@@ -0,0 +1,15 @@
+package br.com.felipe.mvc.sprgmvc2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/oferta")
+public class OfertaController {
+
+	@GetMapping
+	public String getFormularioParaOfertas() {
+		return "oferta/home";
+	}
+}
